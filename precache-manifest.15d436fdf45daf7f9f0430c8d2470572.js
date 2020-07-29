0c9fec4a5f99ec3b3768fbe0b88d81d0e5d90164
singolawu <singolawu.corp@gmail.com>
2020-07-29 16:47:05 +0700

    Updates

---
+++ precache-manifest.15d436fdf45daf7f9f0430c8d2470572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9a850f40a2a5c311506",
+    "url": "/me/static/css/main.c8e087e8.chunk.css"
+  },
+  {
+    "revision": "d9a850f40a2a5c311506",
+    "url": "/me/static/js/main.d9a850f4.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "7b1743f990d5b056f32d",
+    "url": "/me/static/js/2.7b1743f9.chunk.js"
+  },
+  {
+    "revision": "5499c4c209e05412d606acc5de64c787",
+    "url": "/me/index.html"
+  }
+];
